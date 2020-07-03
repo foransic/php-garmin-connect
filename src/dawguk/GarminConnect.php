6c305a656589ef8f427f62331e3bf5464eca6cc4
unknown <Franois Souin>
2020-07-03 15:59:46 +0200

    Update getUsername() so it effectively returns the user name

--- src/dawguk/GarminConnect.php
+++ src/dawguk/GarminConnect.php
@@ -326,7 +326,7 @@ class GarminConnect
         if (!$objUser) {
             return null;
         }
-        return $objUser->username;
+        return json_decode($objUser)->{'username'};
     }
 
     /**
